647cf91682250759b68f5a1cec48d0197b207349
rommel916 <1411818691@qq.com>
2024-11-26 10:08:52 +0800

    feat:模版配置

---
+++ bin/copy.js
@@ -0,0 +1,42 @@
+const copydir = require('copy-dir');
+const fs = require('fs');
+const path = require('path');
+
+function mkdirGuard(target) {
+    try {
+        fs.mkdirSync(target, { recursive: true });
+    } catch (e) {
+        mkdirp(target);
+        function mkdirp(dir) {
+            if (fs.existsSync(dir)) {
+                return true;
+            }
+            const dirname = path.dirname(dir);
+            mkdirp(dirname);
+            fs.mkdirSync(dir);
+        }
+    }
+}
+
+function copyDir(form, to, options) {
+    mkdirGuard(to);
+    copydir.sync(form, to, options);
+}
+
+function checkMkdirExists(path) {
+    return fs.existsSync(path);
+}
+
+function copyFile(from, to) {
+    const buffer = fs.readFileSync(from);
+    const parentPath = path.dirname(to);
+
+    mkdirGuard(parentPath);
+
+    fs.writeFileSync(to, buffer);
+}
+
+exports.checkMkdirExists = checkMkdirExists;
+exports.mkdirGuard = mkdirGuard;
+exports.copyDir = copyDir;
+exports.copyFile = copyFile;
